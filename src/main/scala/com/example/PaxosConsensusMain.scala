8310ec6509bb25232680dbb84e73840816f86d87
kpolunin <kpolunin@griddynamics.com>
2021-01-25 00:34:57 +0100

    fixed bugs

--- src/main/scala/com/example/PaxosConsensusMain.scala
+++ src/main/scala/com/example/PaxosConsensusMain.scala
@@ -3,24 +3,34 @@ package com.example
 import akka.actor.typed.receptionist.Receptionist
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
-import com.example.Replica.{ClusterState, Coordinator}
 
 import scala.concurrent.duration.DurationInt
 
+object Conf {
+  val clusterSize = 9
+  val majQuo = math.ceil(clusterSize / 2.0).toInt + 1
+}
+
 object ReplicaExample {
+
   import com.example.Replica.{ClusterState, Coordinator, TMessage}
 
   object Client {
-    def apply(seed: ActorRef[TMessage]): Behavior[Any] =
+    def apply(seed: ActorRef[TMessage], voteValue: String): Behavior[Any] =
       Behaviors.setup { context =>
-        context.self ! "start"
+        context.self ! Coordinator.InitVote(voteValue, context.self)
         Behaviors.receiveMessage {
-          case _ =>
-            seed ! Coordinator.InitVote("Trump!", context.self)
-            Behaviors.same
           case Coordinator.ClusterOutOfOperability() =>
             import context.executionContext
-            context.system.scheduler.scheduleOnce(2 seconds, () => seed ! Coordinator.InitVote("Trump!", context.self))
+            context.system.scheduler.scheduleOnce(2 seconds, () => seed ! Coordinator.InitVote(voteValue, context.self))
+            Behaviors.same
+
+          case Coordinator.ElectionFinished(value) =>
+            context.log.info("Consensus achieved: {}", value)
+            Behaviors.stopped
+
+          case Coordinator.InitVote(_, _) =>
+            seed ! Coordinator.InitVote(voteValue, context.self)
             Behaviors.same
         }
       }
@@ -30,12 +40,11 @@ object ReplicaExample {
     def apply(): Behavior[Nothing] = {
       Behaviors
         .setup[Receptionist.Listing] { context =>
-          val seed = context.spawn(Replica(), "Replica-1")
-          context.spawn(Replica(), "Replica-2")
-          context.spawn(Replica(), "Replica-3")
+          val cluster = (0 to Conf.clusterSize).map(r => context.spawn(Replica(), "Replica" + r))
 
           context.system.receptionist ! Receptionist.Subscribe(Replica.ReplicaServiceKey, context.self)
-          context.spawn(Client(seed), "Client")
+          context.spawn(Client(cluster.head, "Trump!"), "Client0")
+          context.spawn(Client(cluster.head, "Biden!"), "Client1")
           Behaviors.receiveMessagePartial[Receptionist.Listing] {
             case Replica.ReplicaServiceKey.Listing(listings) =>
               listings.foreach(_ ! ClusterState.ConfigurationChanged(listings))
@@ -45,9 +54,11 @@ object ReplicaExample {
         .narrow
     }
   }
+
 }
 
-object AtomicRegister {
+object SingleDecreePaxos {
+
   import ReplicaExample._
   import akka.actor.typed.ActorSystem
 
--- src/main/scala/com/example/Replica.scala
+++ src/main/scala/com/example/Replica.scala
@@ -3,16 +3,18 @@ package com.example
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
+import com.example.BallotOrdering.toBallotOrdering
 import com.example.Replica.Ballot
 
 import java.time.Instant
 import scala.collection.mutable
 import scala.language.{postfixOps, reflectiveCalls}
 import scala.util.Random
-
+import Conf._
 object Replica {
 
   import BallotOrdering._
+
   final case class Ballot(ballotNumber: Long, guid: String)
 
   final case class Voice(ballot: Ballot, value: String)
@@ -20,49 +22,36 @@ object Replica {
   sealed trait TMessage
 
   object ClusterState {
-
     final case class ConfigurationChanged(replicaSet: Set[ActorRef[TMessage]]) extends TMessage
-
   }
 
   object Coordinator {
-
     final case class InitVote(value: String, sender: ActorRef[TMessage]) extends TMessage
-
     final case class ClusterOutOfOperability() extends TMessage
-
     final case class ElectionFinished(value: String) extends TMessage
-
   }
 
   object Proposer {
-
     final case class Prepare(ballot: Ballot, sender: ActorRef[TMessage]) extends TMessage
-
     final case class Propose(ballot: Ballot, value: String, sender: ActorRef[TMessage]) extends TMessage
-
   }
 
   object Acceptor {
-
     final case class Promise(voice: Option[Voice], sender: ActorRef[TMessage]) extends TMessage
-
     final case class Accepted(voice: Voice, sender: ActorRef[TMessage]) extends TMessage
-
   }
 
-  class ProposerState(var ballot: Option[Ballot], var initialValue: Option[String], val voices: mutable.ListBuffer[Option[Voice]], val acceptedVales: mutable.ListBuffer[Voice])
+  class ProposerState(var ballot: Option[Ballot], var initialValue: Option[String], val voices: mutable.ListBuffer[Option[Voice]], val acceptedVales: mutable.ListBuffer[Voice],var client: ActorRef[TMessage])
 
   class AcceptorState(var biggestBallot: Option[Ballot], var acceptedValue: Option[String])
 
   val ReplicaServiceKey: ServiceKey[TMessage] = ServiceKey[TMessage]("replica")
 
-  def apply(): Behavior[Any] = {
-    Behaviors.setup[Any] { context =>
+  def apply(): Behavior[TMessage] = {
+    Behaviors.setup[TMessage] { context =>
       val guid = java.util.UUID.randomUUID.toString
-      val majQuo = 2
       var replicaSet = Set.empty[ActorRef[TMessage]]
-      val proposerState = new ProposerState(None, None, mutable.ListBuffer.empty[Option[Voice]], mutable.ListBuffer.empty[Voice])
+      val proposerState = new ProposerState(None, None, mutable.ListBuffer.empty[Option[Voice]], mutable.ListBuffer.empty[Voice],context.self)
       val acceptorState = new AcceptorState(None, None)
 
       context.system.receptionist ! Receptionist.Register(ReplicaServiceKey, context.self)
@@ -73,10 +62,12 @@ object Replica {
           case ClusterState.ConfigurationChanged(replicas) =>
             replicaSet = replicas
 
-          case Coordinator.InitVote(value, sender) =>
-            if (replicaSet.size < 3)
+          case msg @ Coordinator.InitVote(value, sender) =>
+            if (replicaSet.size < clusterSize)
               sender ! Coordinator.ClusterOutOfOperability()
             else {
+              context.log.info("Proposer received: {}", msg)
+              proposerState.client = sender
               proposerState.initialValue = Some(value)
               val ballot = Ballot(Instant.now.getEpochSecond, guid)
               proposerState.ballot = Some(ballot)
@@ -87,6 +78,7 @@ object Replica {
             }
 
           case Proposer.Prepare(ballot, sender) =>
+            context.log.info("Acceptor received: {}", Proposer.Prepare(ballot, sender))
             acceptorState.biggestBallot match {
               case None =>
                 acceptorState.biggestBallot = Some(ballot)
@@ -102,11 +94,18 @@ object Replica {
                 }
             }
 
-          case Acceptor.Promise(voice, sender) =>
+          case msg @ Acceptor.Promise(voice, sender) =>
+            context.log.info("Proposer received: {}", msg)
             proposerState.voices.addOne(voice)
             if (proposerState.voices.size == majQuo) {
-              implicit val userOrdering: Ordering[Ballot] = Ordering.by(_.ballotNumber)
-              val biggest: Voice = proposerState.voices.maxBy(r => r.get.ballot) match {
+
+              implicit val voiceOrdering: Ordering[Option[Voice]] = {
+                case (Some(v), Some(v2)) => if (v.ballot > v2.ballot) 1 else if (v.ballot == v2.ballot) 0 else -1
+                case (Some(_), None) => 1
+                case (None, Some(_)) => -1
+                case _ => 0
+              }
+              val biggest: Voice = proposerState.voices.maxBy(r => r) match {
                 case None => Voice(proposerState.ballot.get, proposerState.initialValue.get)
                 case Some(voice) => Voice(proposerState.ballot.get, voice.value)
               }
@@ -117,28 +116,41 @@ object Replica {
             }
 
           case Proposer.Propose(ballot, value, sender) =>
-
-            if (ballot > ballot) {
+            context.log.info("Acceptor received: {}", Proposer.Propose(ballot, value, sender))
+            if (acceptorState.biggestBallot.forall(r => ballot >= r)) {
               acceptorState.biggestBallot = Some(ballot)
               acceptorState.acceptedValue = Some(value)
               sender ! Acceptor.Accepted(Voice(ballot, value), context.self)
             }
 
-          case Acceptor.Accepted(voice, sender) =>
+          case msg @ Acceptor.Accepted(voice, sender) =>
+            context.log.info("Proposer received: {}", msg)
             proposerState.acceptedVales.addOne(voice)
+            if(proposerState.acceptedVales.size == majQuo) {
+                 proposerState.acceptedVales.headOption.foreach(v=> proposerState.client ! Coordinator.ElectionFinished(v.value))
+            }
         }
         Behaviors.same
       }
     }.narrow
   }
 
-
 }
 
 class BallotOrdering(val bal: Ballot) {
-  def >(other: Ballot): Boolean = bal.ballotNumber > other.ballotNumber
+  def ==(other: Ballot): Boolean =
+    bal.ballotNumber == other.ballotNumber && bal.guid == other.guid
+
+  def >(other: Ballot): Boolean =
+    if (bal.ballotNumber == other.ballotNumber)
+      bal.guid > other.guid
+    else
+      bal.ballotNumber > other.ballotNumber
+
+  def >=(other: Ballot): Boolean =
+    bal > other || bal == other
 }
 
 object BallotOrdering {
-  implicit def fromInt(n: Ballot): BallotOrdering = new BallotOrdering(n)
+  implicit def toBallotOrdering(n: Ballot): BallotOrdering = new BallotOrdering(n)
 }
